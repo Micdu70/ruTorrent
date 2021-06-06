0456630a9da976251d648c18a297d6d7f1ee0c9c
Micdu70 <Micdu70@gmx.fr>
2021-06-07 00:00:23 +0200

    update speedgraph plugin

---
+++ plugins/speedgraph/lang/fr.js
@@ -0,0 +1,11 @@
+/*
+ * PLUGIN Speed Graph
+ *
+ * French language file.
+ *
+ * Author: Micdu70
+ */
+ theUILang.spdGraphLength = "Durée de surveillance";
+ theUILang.spdGraphName = "Graphique de vitesse";
+
+thePlugins.get("speedgraph").langLoaded();
